464ee2a8804e17596e9b73e0ae1157318ea70ca3
3dharmadev <69399810+3dharmadev@users.noreply.github.com>
2022-07-04 21:38:53 +0530

    file name change successfully

--- Object method/problem2.js
+++ Object method/problem2.js
@@ -1,63 +1,52 @@
-// Given information in a data i.e contain the keys as name, toUpperCase 
-// and toLowercase.
-// toUpperCase : function to print the uppercase of the given name
-// toLowerCase : function to print the lowercase of the given name
 
-data = {
+let student_details=
+[
+	{
+		grade: "V",
+		students: [
+			{name: "Nrupul", marks: [10, 20, 30]},                                        
+			{name: "Prateek", marks: [20, 30, 40]}
+		]
+	},
+	{
+		grade: "VI",
+		students: [
+			{name: "Aman", marks: [10, 20, 30]},
+			{name: "Albert", marks: [20, 30, 40]}
+		]
+	},
+	{
+		grade: "VII",
+		students: [
+			{name: "Yogesh", marks: [10, 20, 30]},
+			{name: "Sandhya", marks: [20, 30, 40]}
+		]
+	}
+];
 
-					name : "Suraj Lal Singh",
-					toUpperCase : function(){
-                        let bag="";
-							let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                            let lower="abcdefghijklmnopqrstuvwxyz";
-                            for(let i=0;i<this.name.length;i++){
-                                let bhai="nehin mila";
-                                let value;
-                                for(let j=0;j<upper.length;j++){
-                                    if(this.name[i]==lower[j]){
-                                      bhai="milgeya";
-                                       value=upper[j];
-                                    }
-                                
-                                }
-                                if(bhai=="nehin mila"){
-                                    bag=bag+this.name[i];
-                                }
-                                else{
-                                    bag=bag+value;
-                                }
-                               
-                        
-                            }
-                            console.log(bag);					
-						},
-					toLowerCase : function(){
-		            
-                        let bag="";
-                        let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                        let lower="abcdefghijklmnopqrstuvwxyz";
-                        for(let i=0;i<this.name.length;i++){
-                            let bhai="nehin mila";
-                            let value;
-                            for(let j=0;j<upper.length;j++){
-                                if(this.name[i]==upper[j]){
-                                  bhai="milgeya";
-                                   value=lower[j];
-                                }
-                            
-                            }
-                            if(bhai=="nehin mila"){
-                                bag=bag+this.name[i];
-                            }
-                            else{
-                                bag=bag+value;
-                            }
-                           
-                    
-                        }
-                        console.log(bag);	
 
-						 }
-			}
-            data.toUpperCase();
-            data.toLowerCase();
+function marks_array(para){
+    sum=0;
+    for(let i=0;i<para.length;i++){
+      sum=sum+para[i];
+    }
+    return sum;
+}
+
+function brilliant_students_details(info){
+  let student_grade=info.grade;
+  let max=-Infinity;
+  let name;
+  for(let i=0;i<info["students"].length;i++){
+    let total_marks=marks_array(info.students[i].marks);
+     if(max<total_marks){
+       name=info.students[i].name;
+        max=total_marks;
+     }
+  }
+  console.log(student_grade+"-"+name+"-"+max);
+
+}
+for(let i=0;i<student_details.length;i++){
+    brilliant_students_details(student_details[i]);
+}
